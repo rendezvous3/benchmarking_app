13e7a29e681bbbbec9f8d66eeb70835af018bcb5
rendezvous3 <bojanj6m@gmail.com>
2017-10-23 13:31:23 -0400

    Added onChange and onSubmit form handlers

--- client/components/UserIdForm.js
+++ client/components/UserIdForm.js
@@ -1,22 +1,52 @@
 import React, { Component } from 'react'
 import {connect} from 'react-redux'
 import PropTypes from 'prop-types'
-import {auth} from '../store'
+import { fetchSimilarUsers } from '../store'
 
 class UserIdForm extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      userId: null,
+    }
+
+    this.handleChangeId = this.handleChangeId.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChangeId(evt){
+    const userId = Number(evt.target.value)
+    this.setState({ userId })
+  }
+
+  handleSubmit(evt){
+    evt.preventDefault()
+    console.log(this.state.userId)
+  }
+
   render(){
-    return (<form>
+    console.log(this.state)
+    return (<form onSubmit={this.handleSubmit}>
               <h4>Enter your Id</h4>
               <hr/>
-              <input type="text" />
+              <input type="text" onChange={this.handleChangeId}/>
+              <button>Find</button>
             </form>)
   }
 }
 
-const MapState = () => {
+const MapState = (state) => {
   return {
 
   }
 }
 
-export default connect(MapState)(UserIdForm)
+const MapDispatch = (dispatch) => {
+  return {
+    getSimilarUsers: (user_id) => {
+      dispatch(fetchSimilarUsers(user_id))
+    }
+  }
+}
+
+export default connect(MapState, MapDispatch)(UserIdForm)
